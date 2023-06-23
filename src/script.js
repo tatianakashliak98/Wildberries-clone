0fc75f0be211f91aabe7dc93df78595fbf95b206
daryapatsko <darya.patsko@mail.ru>
2023-06-23 23:13:01 +0300

    add slider-swiper

---
+++ src/script.js
@@ -0,0 +1,13 @@
+let swiper = new Swiper('.swiper-container',{
+    spaceBetween:30,
+    slidesPerView:1,
+    navigation:{
+        nextEl:'.swiper-button-next',
+        prevEl:'.swiper-button-prev',
+    },
+    slidesPerView:1,
+    scrollbar:{
+        el:'.swiper-scrollbar',
+        draggable:true,
+    },
+});
