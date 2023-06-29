59b087422e12d70f30ce3f8cb820c336d22cc7bc
Nadya <nadya.voronova@gmail.com>
2023-06-29 15:36:12 +0200

    add modalWindow on cards

--- src/script.js
+++ src/script.js
@@ -13,3 +13,52 @@ let swiper = new Swiper('.swiper',{
     loop:true,
    
 });
+
+let modalClose = document.getElementsByClassName("modal-close")[0];
+modalClose.onclick = function() {
+    let modal = document.getElementById("myModal");
+    modal.style.display = "none"; // Скрываем модальное окно
+}
+
+let mainContainer = document.querySelector("main");
+
+mainContainer.addEventListener('click', modalWindow);
+
+function modalWindow({target}) {
+  let nameClass = target.getAttribute("class");
+  if (nameClass === "view__container" ) {
+    let card = target.closest('.card');
+    let title = card.querySelector(".card__name").innerHTML;
+    let imageSrc = target.previousElementSibling.src;
+    let price = card.querySelector(".price").innerHTML;
+  let modal = document.getElementById("myModal");
+  let modalTitle = document.getElementById("modal-title");
+  let modalImage = document.getElementById("modal-image");
+  let modalPrice = document.getElementById("modal-price");
+
+  modalTitle.innerHTML = title;
+  modalImage.src = imageSrc;
+  modalPrice.innerHTML = price;
+  
+  modal.style.display = "block";
+
+  } else if (nameClass === "view__card-btn") {
+    let card = target.closest('.card');
+    let title = card.querySelector(".card__name").innerHTML;
+    let price = card.querySelector(".price").innerHTML;
+    let imageSrc = target.parentElement.previousElementSibling.src;
+
+    let modal = document.getElementById("myModal");
+  let modalTitle = document.getElementById("modal-title");
+  let modalImage = document.getElementById("modal-image");
+  let modalPrice = document.getElementById("modal-price");
+
+  modalTitle.innerHTML = title;
+  modalImage.src = imageSrc;
+  modalPrice.innerHTML = price;
+  
+  modal.style.display = "block";
+
+  }
+}
+
