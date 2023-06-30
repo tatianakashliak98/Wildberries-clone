bd14da5dd65b89cde0e95b7f178f1111f0ab4d57
Nadya <nadya.voronova@gmail.com>
2023-06-30 11:49:49 +0200

    fix: add card_modal_window, fix classes

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+import { modalWindow, close__modal } from "./card_modal_window.js";
+
 let swiper = new Swiper('.swiper',{
     spaceBetween:300,
     slidesPerView:1,
@@ -14,51 +16,7 @@ let swiper = new Swiper('.swiper',{
    
 });
 
-let modalClose = document.getElementsByClassName("modal-close")[0];
-modalClose.onclick = function() {
-    let modal = document.getElementById("myModal");
-    modal.style.display = "none"; // Скрываем модальное окно
-}
-
 let mainContainer = document.querySelector("main");
-
 mainContainer.addEventListener('click', modalWindow);
-
-function modalWindow({target}) {
-  let nameClass = target.getAttribute("class");
-  if (nameClass === "view__container" ) {
-    let card = target.closest('.card');
-    let title = card.querySelector(".card__name").innerHTML;
-    let imageSrc = target.previousElementSibling.src;
-    let price = card.querySelector(".price").innerHTML;
-  let modal = document.getElementById("myModal");
-  let modalTitle = document.getElementById("modal-title");
-  let modalImage = document.getElementById("modal-image");
-  let modalPrice = document.getElementById("modal-price");
-
-  modalTitle.innerHTML = title;
-  modalImage.src = imageSrc;
-  modalPrice.innerHTML = price;
-  
-  modal.style.display = "block";
-
-  } else if (nameClass === "view__card-btn") {
-    let card = target.closest('.card');
-    let title = card.querySelector(".card__name").innerHTML;
-    let price = card.querySelector(".price").innerHTML;
-    let imageSrc = target.parentElement.previousElementSibling.src;
-
-    let modal = document.getElementById("myModal");
-  let modalTitle = document.getElementById("modal-title");
-  let modalImage = document.getElementById("modal-image");
-  let modalPrice = document.getElementById("modal-price");
-
-  modalTitle.innerHTML = title;
-  modalImage.src = imageSrc;
-  modalPrice.innerHTML = price;
-  
-  modal.style.display = "block";
-
-  }
-}
-
+let modalClose = document.querySelector(".modal__close");
+modalClose.addEventListener('click', close__modal);
