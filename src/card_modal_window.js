b1cdeed05bf703ce7e93514203a9c878f33a2826
Nadya <nadya.voronova@gmail.com>
2023-07-02 16:41:03 +0200

    fix: fix modal windows, add newPrice in api.js

--- src/card_modal_window.js
+++ src/card_modal_window.js
@@ -1,39 +1,29 @@
-
-
-export function close__modal(){
-    let modal = document.getElementById("card__preview");
-    modal.style.display = "none"; 
+export function close__modal() {
+  let modal = document.getElementById("card__preview");
+  modal.style.display = "none";
 }
 
-
-export function modalWindow({target}) {
+export function modalWindow({ target }) {
   let nameClass = target.getAttribute("class");
   let modal = document.getElementById("card__preview");
   let modalTitle = document.getElementById("modal__title");
   let modalImage = document.getElementById("modal__image");
   let modalPrice = document.getElementById("modal__price");
-  if (nameClass === "view__container" ) {
-    let card = target.closest('.card');
-    let title = card.querySelector(".card__name").innerHTML;
-    let imageSrc = target.previousElementSibling.src;
-    let price = card.querySelector(".price").innerHTML;
-  modalTitle.innerHTML = title;
-  modalImage.src = imageSrc;
-  modalPrice.innerHTML = price;
-  
-  modal.style.display = "block";
-
-  } else if (nameClass === "view__card-btn") {
-    let card = target.closest('.card');
+  if (
+    nameClass === "view__container" ||
+    nameClass === "view__card-btn" ||
+    nameClass === "hide__view__card-btn"||
+    nameClass === "card__name"
+  ) {
+    let card = target.closest(".card");
     let title = card.querySelector(".card__name").innerHTML;
     let price = card.querySelector(".price").innerHTML;
-    let imageSrc = target.parentElement.previousElementSibling.src;
+    let imageSrc = card.querySelector(".product-img").src;
 
-  modalTitle.innerHTML = title;
-  modalImage.src = imageSrc;
-  modalPrice.innerHTML = price;
-  
-  modal.style.display = "block";
+    modalTitle.innerHTML = title;
+    modalImage.src = imageSrc;
+    modalPrice.innerHTML = price;
 
+    modal.style.display = "block";
   }
 }
